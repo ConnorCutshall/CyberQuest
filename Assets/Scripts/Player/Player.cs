fdb69ff4a763dae88034110bbbfd1531d09d2b7f
ConnorCutshall <connorlcutshall@gmail.com>
2023-04-06 17:42:08 -0400

    Updated

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cam;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("q"))
+        {
+            //cam.GetComponent<CinemachineVirtualCamera>();
+        }
+        else 
+        { 
+
+        }
+    }
+}
