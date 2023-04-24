deea56f37bba3fde6813b2f68c39259635945e88
ConnorCutshall <connorlcutshall@gmail.com>
2023-04-24 12:52:15 -0400

    Updated 4/24

--- Assets/Scripts/ScriptableObj/UpgradeData.cs
+++ Assets/Scripts/ScriptableObj/UpgradeData.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New UpgradeData Object", menuName = "Data/UpgradeData")]
 public class UpgradeData : ScriptableObject
 {
+    public int playerMoney;
+
     public int FireWallLevel;
     public int VPNLevel;
 }
--- Assets/Scripts/Skill/SkillNode.cs
+++ Assets/Scripts/Skill/SkillNode.cs
@@ -8,63 +8,108 @@ public class SkillNode : MonoBehaviour
 {
     public string skillName;
     public List<GameObject> nextSkills;
-    public List<GameObject> lines;
+    //public List<GameObject> lines;
     public SkillNode prevSkill;
 
     public UpgradeData upgradeData;
 
     public bool canUnlock;
     public bool isUnlocked;
+    public int nextLevelUnlockCost;
 
-    public TMP_Text level;
+    public TMP_Text costText;
+    public TMP_Text levelText;
+    public TMP_Text moneyText;
     public NodeType nodeType;
 
+    public int level;
     public Button button;
-    public Color unlockedColor;
-    public Color lockedColor;
+
+    //public Color unlockedColor;
+    //public Color lockedColor;
 
     public enum NodeType {VPN, FireWall }
 
     public void Start()
     {
+        //setup button
         button = GetComponent<Button>();
+        button.onClick.AddListener(buttonPush);
+        //update level string text
         switch (nodeType)
         {
             case NodeType.VPN:
-                level.text = upgradeData.VPNLevel.ToString();
+                level = upgradeData.VPNLevel;
                 break;
             case NodeType.FireWall:
-                level.text = upgradeData.FireWallLevel.ToString();
+                level = upgradeData.FireWallLevel;
                 break;
         }
 
+        //push Update
         pushUpdate();
     }
     public void Update()
     {
-        button.onClick.AddListener(buttonPush);
+        //button setup was here
     }
     void pushUpdate() 
     {
-        if (this.isUnlocked)
+        //update level text
+        levelText.text = level.ToString();
+        moneyText.text = "$" + upgradeData.playerMoney.ToString();
+        if (level == 10)
         {
-            this.GetComponent<Image>().color = unlockedColor;
-            foreach (GameObject line in lines) 
-            {
-                line.GetComponent<Image>().color = unlockedColor;
-            }
+            costText.gameObject.SetActive(false);
         }
         else 
         {
-            this.GetComponent<Image>().color = lockedColor;
-            foreach (GameObject line in lines)
-            {
-                line.GetComponent<Image>().color = lockedColor;
-            }
+            costText.text = "$" + nextLevelUnlockCost;
         }
+
+        if (level == 0) 
+        {
+            levelText.gameObject.SetActive(false);
+        }
+
+        if (level == 1) 
+        {
+            levelText.gameObject.SetActive(true);
+        }
+
+        // updates color of node and corresponding lines
+        //if (this.isUnlocked)
+        //{
+        //    this.GetComponent<Image>().color = unlockedColor;
+        //    foreach (GameObject line in lines) 
+        //    {
+        //        line.GetComponent<Image>().color = unlockedColor;
+        //    }
+        //}
+        //else 
+        //{
+        //    this.GetComponent<Image>().color = lockedColor;
+        //    foreach (GameObject line in lines)
+        //    {
+        //        line.GetComponent<Image>().color = lockedColor;
+        //    }
+        //}
+
+        //updates inormation in sriptable object
+        switch (nodeType)
+        {
+            case NodeType.VPN:
+                upgradeData.VPNLevel = level;
+                break;
+            case NodeType.FireWall:
+                upgradeData.FireWallLevel = level;
+                break;
+        }
+
     }
     void buttonPush() 
     {
+        //test to see if the node can be unlocked
         if (!isUnlocked && canUnlock) 
         {
             isUnlocked = true;
@@ -72,8 +117,15 @@ public class SkillNode : MonoBehaviour
             {
                 skill.GetComponent<SkillNode>().canUnlock = true;
             }
-            pushUpdate();
         }
-    }
 
+        if (upgradeData.playerMoney >= nextLevelUnlockCost
+            && level < 10) 
+        {
+            level++;
+            upgradeData.playerMoney -= nextLevelUnlockCost;
+            nextLevelUnlockCost += 50;
+        }
+        pushUpdate();
+    }
 }
